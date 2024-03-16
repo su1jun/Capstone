9200ed9483177ac1095725acdd34a7855456f66b
su1jun <qaxsd530@gmail.com>
2024-03-16 09:45:12 +0000

    1

---
+++ ros2_ws/build/robot/build/lib/robot/gps_test.py
@@ -0,0 +1,48 @@
+#import the necessary libraries
+from gps import *
+import time
+from datetime import datetime
+import pytz, dateutil.parser
+
+
+running = True
+
+def getPositionData(gps):
+    nx = gpsd.next()
+    
+    if nx['class'] == 'TPV':
+        #extract gps info
+        latitude = getattr(nx,'lat', "Unknown")
+        longitude = getattr(nx,'lon', "Unknown")
+        speed=getattr(nx,'speed', "Unknown")
+
+        #change directory to wherever you want to txt file to be located
+        with open(r'/home/su1jun/GPS/log/newoutput.txt', "a+") as file_object:
+            # Move read cursor to the start of file.
+            file_object.seek(0)
+            # If file is not empty then append '\n'
+            data = file_object.read(100)
+            if len(data) > 0 :
+                file_object.write("\n")
+            #stores the current date/time
+            dateTimeObj=datetime.now()
+            info= f"Time: {dateTimeObj}, Position: (lon={longitude}, lat={latitude}), Speed: {speed} km/h"
+            print (info)
+            #Append gps info at the end of the file
+            file_object.write(info)
+            
+# Tell gpsd we are ready to receive messages
+gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE)
+
+
+try:
+    print ("Application started!")
+    while running:
+        #call function to extract and append GPS data
+        getPositionData(gpsd)
+        #delay running the program for 1 second
+        time.sleep(1)
+        
+#if the user presses ctrl+c, the program will stop running
+except (KeyboardInterrupt):
+    running = False
---
+++ ros2_ws/install/robot/lib/python3.10/site-packages/robot/const.py
@@ -0,0 +1,16 @@
+START_SPEED = 0
+STRAIGHT_SPEED = 50
+TURN_SPEED = 50
+
+SH = False # for debug, sh = True
+
+# pin number
+PWMA = 18
+PWMB = 23
+AIN = [22, 27]
+BIN = [25, 24]
+LED = [26, 16, 20, 21]
+SW = [5, 6, 13, 19]
+BUZZER = 12
+
+BLUETHOOTH = "/dev/ttyS0"
