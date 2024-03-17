fd14f27f9295ca17f16611a9b9279e144cf54af2
su1jun <qaxsd530@gmail.com>
2024-03-17 15:01:13 +0900

    1

--- ros2_ws/src/robot/robot/motor_node.py
+++ ros2_ws/src/robot/robot/motor_node.py
@@ -1,220 +1,46 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String  # Replace with appropriate message type
-import RPi.GPIO as GPIO
-from .const import *
+from geometry_msgs.msg import Twist
+from chip_bldc_driver.msg import Command # 모터 드라이버에 맞는 메시지 타입으로 변경
 
-class MotorNode(Node):
-    def __init__(self, test):
-        super().__init__('motor_control_node')
-
-        self.setup_gpio(test)
-        # Update topic and message type
-        self.subscription_switch = self.create_subscription(
-            String, 
-            'switch_topic', 
-            self.listener_callback_1, 
+class MotorSpeedController(Node):
+    def __init__(self):
+        super().__init__('motor_speed_controller')
+        self.subscription = self.create_subscription(
+            Twist,
+            '/cmd_vel',
+            self.listener_callback,
             10)
-        self.subscription_bluetooth = self.create_subscription(
-            String, 
-            'bluetooth_topic', 
-            self.listener_callback_2, 
-            10)
-        
-        self.get_logger().info('Motor Node has been started')
+        self.subscription # prevent unused variable warning
 
-    def __del__(self):
-        pass
+        self.left_motor_pub = self.create_publisher(Command, 'left/motor_command', 10)
+        self.right_motor_pub = self.create_publisher(Command, 'right/motor_command', 10)
 
-    # def timer_callback(self):
-    #     # Implement logic here
-    #     msg = String()  # Update with appropriate message type
-    #     # Update msg data
-    #     self.publisher_.publish(msg)
+    def listener_callback(self, msg):
+        # 여기서는 간단히 선형 속도만을 사용하여 모터 명령을 생성합니다.
+        # 실제 로봇의 구조에 따라 적절히 조정해야 합니다.
+        left_command = Command()
+        right_command = Command()
 
-    def listener_callback_1(self, msg):
-        self.get_logger().info(str(msg))
-        switch_state = msg.data
-        if switch_state == 'go':
-            self.motor_go(STRAIGHT_SPEED, STRAIGHT_SPEED)
-        elif switch_state == 'left':
-            self.motor_left(TURN_SPEED)
-        elif switch_state == 'right':
-            self.motor_right(TURN_SPEED)
-        elif switch_state == 'back':
-            self.motor_back(STRAIGHT_SPEED)
-        elif switch_state == 'stop':
-            self.motor_stop()
+        # 선형 속도를 모터 명령으로 변환하는 로직을 구현합니다.
+        # 예를 들어, 선형 속도를 모터의 RPM으로 변환하는 경우 다음과 같이 할 수 있습니다.
+        # 이 부분은 모터 드라이버의 명령 형식에 따라 변경해야 합니다.
+        left_command.motor_command = msg.linear.x * 1000 # 예시: 선형 속도를 RPM으로 변환
+        right_command.motor_command = msg.linear.x * 1000 # 예시: 선형 속도를 RPM으로 변환
 
-    def listener_callback_2(self, msg):
-        self.get_logger().info(str(msg))
-        bluetooth_state = msg.data
-        if bluetooth_state == 'go':
-            self.motor_go(STRAIGHT_SPEED, STRAIGHT_SPEED)
-        elif bluetooth_state == 'left':
-            self.motor_left(TURN_SPEED)
-        elif bluetooth_state == 'right':
-            self.motor_right(TURN_SPEED)
-        elif bluetooth_state == 'back':
-            self.motor_back(STRAIGHT_SPEED)
-        elif bluetooth_state == 'stop':
-            self.motor_stop()
-        # elif bluetooth_state == 'buzz_on':
-        #     self.buzz_on()
-        # elif bluetooth_state == 'bz_off':
-        #     self.buzz_off()
-
-    def setup_gpio(self, test=False):
-        self.SH = test
-        
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BCM)
-        
-        GPIO.setup(PWMA, GPIO.OUT)
-        GPIO.setup(AIN[0], GPIO.OUT)
-        GPIO.setup(AIN[1], GPIO.OUT)
-        
-        GPIO.setup(PWMB, GPIO.OUT)
-        GPIO.setup(BIN[0], GPIO.OUT)
-        GPIO.setup(BIN[1], GPIO.OUT)
-
-        GPIO.setup(LED[0], GPIO.OUT)
-        GPIO.setup(LED[1], GPIO.OUT)
-        GPIO.setup(LED[2], GPIO.OUT)
-        GPIO.setup(LED[3], GPIO.OUT)
-
-        self.L_Motor = GPIO.PWM(PWMA,500)
-        self.L_Motor.start(0)
-
-        self.R_Motor = GPIO.PWM(PWMB,500)
-        self.R_Motor.start(0)
-
-        GPIO.output(LED[0], GPIO.HIGH)
-        GPIO.output(LED[1], GPIO.HIGH)
-        GPIO.output(LED[2], GPIO.LOW)
-        GPIO.output(LED[3], GPIO.LOW)
-
-        GPIO.output(AIN[0], 0)
-        GPIO.output(AIN[1], 1)
-        GPIO.output(BIN[0], 0)
-        GPIO.output(BIN[1], 1)
-        
-        if not self.SH:
-            self.L_Motor.ChangeDutyCycle(START_SPEED)
-            self.R_Motor.ChangeDutyCycle(START_SPEED)
-
-    def motor_go(self, l_speed, r_speed):
-        if l_speed > r_speed:
-            GPIO.output(LED[0],GPIO.HIGH)
-            GPIO.output(LED[1],GPIO.LOW)
-            GPIO.output(LED[2],GPIO.HIGH)
-            GPIO.output(LED[3],GPIO.LOW)
-
-        elif l_speed < r_speed:
-            GPIO.output(LED[0],GPIO.LOW)
-            GPIO.output(LED[1],GPIO.HIGH)
-            GPIO.output(LED[2],GPIO.LOW)
-            GPIO.output(LED[3],GPIO.HIGH)
-
-        else:
-            GPIO.output(LED[0],GPIO.HIGH)
-            GPIO.output(LED[1],GPIO.HIGH)
-            GPIO.output(LED[2],GPIO.LOW)
-            GPIO.output(LED[3],GPIO.LOW)
-
-        if l_speed >= 0:
-            GPIO.output(AIN[0], 0)
-            GPIO.output(AIN[1], 1)
-        else:
-            GPIO.output(AIN[0], 1)
-            GPIO.output(AIN[1], 0)
-            l_speed = -l_speed 
-        
-        if r_speed >= 0:
-            GPIO.output(BIN[0], 0)
-            GPIO.output(BIN[1], 1)
-        else:
-            GPIO.output(BIN[0], 1)
-            GPIO.output(BIN[1], 0)
-            r_speed = -r_speed 
-            
-        l_speed = min(l_speed, 100)
-        r_speed = min(r_speed, 100)
-        
-        if not self.SH:
-            self.L_Motor.ChangeDutyCycle(l_speed)
-            self.R_Motor.ChangeDutyCycle(r_speed)
-             
-    def motor_left(self, speed=100):
-        GPIO.output(LED[0],GPIO.HIGH)
-        GPIO.output(LED[1],GPIO.LOW)
-        GPIO.output(LED[2],GPIO.HIGH)
-        GPIO.output(LED[3],GPIO.LOW)
-
-        GPIO.output(AIN[0],1)
-        GPIO.output(AIN[1],0)
-        GPIO.output(BIN[0],0)
-        GPIO.output(BIN[1],1)
-
-        if not self.SH:
-            self.L_Motor.ChangeDutyCycle(speed)
-            self.R_Motor.ChangeDutyCycle(speed)
-        
-    def motor_right(self, speed=100):
-        GPIO.output(LED[0],GPIO.LOW)
-        GPIO.output(LED[1],GPIO.HIGH)
-        GPIO.output(LED[2],GPIO.LOW)
-        GPIO.output(LED[3],GPIO.HIGH)
-        
-        GPIO.output(AIN[0],0)
-        GPIO.output(AIN[1],1)
-        GPIO.output(BIN[0],1)
-        GPIO.output(BIN[1],0)
-
-        if not self.SH:
-            self.L_Motor.ChangeDutyCycle(speed)
-            self.R_Motor.ChangeDutyCycle(speed)
-
-    def motor_back(self, speed=100):
-        GPIO.output(LED[0],GPIO.LOW)
-        GPIO.output(LED[1],GPIO.LOW)
-        GPIO.output(LED[2],GPIO.HIGH)
-        GPIO.output(LED[3],GPIO.HIGH)
-        
-        GPIO.output(AIN[0], 1)
-        GPIO.output(AIN[1], 0)
-        GPIO.output(BIN[0], 1)
-        GPIO.output(BIN[1], 0)
-
-        if not self.SH:
-            self.L_Motor.ChangeDutyCycle(speed)
-            self.R_Motor.ChangeDutyCycle(speed)
-
-    def motor_stop(self):
-        GPIO.output(LED[0],GPIO.LOW)
-        GPIO.output(LED[1],GPIO.LOW)
-        GPIO.output(LED[2],GPIO.LOW)
-        GPIO.output(LED[3],GPIO.LOW)
-        
-        GPIO.output(AIN[0],0)
-        GPIO.output(AIN[1],1)
-        GPIO.output(BIN[0],0)
-        GPIO.output(BIN[1],1)
-
-        if not self.SH:
-            self.L_Motor.ChangeDutyCycle(0)
-            self.R_Motor.ChangeDutyCycle(0)
+        self.left_motor_pub.publish(left_command)
+        self.right_motor_pub.publish(right_command)
 
 def main(args=None):
     rclpy.init(args=args)
-    motor_control_node = MotorNode(SH)
 
-    rclpy.spin(motor_control_node)
+    motor_speed_controller = MotorSpeedController()
 
-    motor_control_node.destroy_node()
+    rclpy.spin(motor_speed_controller)
+
+    # Destroy the node explicitly
+    motor_speed_controller.destroy_node()
     rclpy.shutdown()
-    GPIO.cleanup()
 
 if __name__ == '__main__':
     main()
-    
