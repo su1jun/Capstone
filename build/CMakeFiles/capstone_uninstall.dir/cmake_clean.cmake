1250f6efa8111acc451e23c3d3c1f86afc15d487
su1jun <qaxsd530@gmail.com>
2024-03-18 20:05:36 +0900

    1

---
+++ build/CMakeFiles/capstone_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/capstone_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/capstone_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
