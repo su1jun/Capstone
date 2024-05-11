6f516fda6dca5c2e98c9d1b393d1501864aa7644
su1jun <qaxsd530@gmail.com>
2024-05-11 20:33:18 +0900

    1

--- spring_ws/src/main/java/com/example/spring_ws/controller/ShellController.java
+++ spring_ws/src/main/java/com/example/spring_ws/controller/ShellController.java
@@ -1,4 +1,19 @@
 package com.example.spring_ws.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.shell.Shell;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ShellController {
+
+    @Autowired
+    private Shell shell;
+
+    @PostMapping("/shell")
+    public String executeCommand(@RequestBody String command) {
+        return (String) shell.evaluate(() -> command);
+    }
 }
--- spring_ws/src/main/java/com/example/spring_ws/temp/CustomCommands.java
+++ spring_ws/src/main/java/com/example/spring_ws/temp/CustomCommands.java
@@ -1,4 +1,18 @@
 package com.example.spring_ws.temp;
 
-public class Customcommands {
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+@ShellComponent
+public class CustomCommands {
+
+    @ShellMethod("Say hello")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+
+    @ShellMethod("Add two integers")
+    public int add(int a, int b) {
+        return a + b;
+    }
 }
