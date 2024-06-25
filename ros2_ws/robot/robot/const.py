21841751790bcbfc904472e052a9f146e7f159ef
su1jun <qaxsd530@gmail.com>
2024-06-25 14:59:43 +0900

    1

---
+++ ros2_ws/robot/robot/const.py
@@ -0,0 +1,19 @@
+START_SPEED = 0
+STRAIGHT_SPEED = 50
+TURN_SPEED = 50
+
+SH = False # for debug, sh = True
+
+# pin number
+PWMA = 18
+PWMB = 23
+AIN = [22, 27]
+BIN = [25, 24]
+LED = [26, 16, 20, 21]
+SW = [5, 6, 13, 19]
+BUZZER = 12
+
+BLUETHOOTH = "/dev/ttyS0"
+
+P = 35 # ratio to distance between the wheels
+K = 180 # ratio to wheel`s radius
